d2a3d1b86f44406709b16f7209d18cf999f20a7d
MorenoAlej0 <morenoalejo.cba@gmail.com>
2026-08-27 19:09:16 -0300

    feat: migrar dashboard admin de maqueta estática a componente Angular

--- frontend/gestor-inventario-frontend/src/app/app.routes.ts
+++ frontend/gestor-inventario-frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DashboardAdmin } from './pages/dashboard-admin/dashboard-admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "dashboardAdmin", component: DashboardAdmin },
+];
