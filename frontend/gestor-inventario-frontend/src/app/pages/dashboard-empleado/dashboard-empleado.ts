81f81387d052f2311c86bb91ec73b378a901ad59
MateCooked <mattecooked@gmail.com>
2026-08-30 00:47:17 -0300

    [Matias] feat: generar componente dashboard-empleado

---
+++ frontend/gestor-inventario-frontend/src/app/pages/dashboard-empleado/dashboard-empleado.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-empleado',
+  imports: [],
+  templateUrl: './dashboard-empleado.html',
+  styleUrl: './dashboard-empleado.css',
+})
+export class DashboardEmpleado {}
