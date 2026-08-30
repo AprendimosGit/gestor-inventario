b13cf24b79d677c7d098f84d1e316b8f0ddfafc0
MateCooked <mattecooked@gmail.com>
2026-08-30 01:57:57 -0300

    [Matias] feat: se crea  stock-empleado con ruta hija y redireccion por defecto a la misma

--- frontend/gestor-inventario-frontend/src/app/app.routes.ts
+++ frontend/gestor-inventario-frontend/src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { DashboardAdmin } from './pages/dashboard-admin/dashboard-admin';
 import { DashboardEmpleado } from './pages/dashboard-empleado/dashboard-empleado';
+import { StockEmpleado } from './pages/dashboard-empleado/stock-empleado/stock-empleado';
 
 export const routes: Routes = [
     { path: "dashboardAdmin", component: DashboardAdmin },
-    { path: "dashboardEmpleado", component: DashboardEmpleado}
+    { 
+        path: "dashboardEmpleado",
+        component: DashboardEmpleado,
+        children: [
+            { path: "", redirectTo: "stock", pathMatch: "full" },
+            { path: "stock", component: StockEmpleado },
+        ],
+    },
 ];
