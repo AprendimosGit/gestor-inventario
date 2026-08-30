f4c233bdae01c8eced24666cfc449568c09901b5
fernandallanos13 <llanosfernanda1@gmail.com>
2026-08-29 21:02:15 -0300

    feat: migrar Home y Quienes Somos a componentes Angular

--- frontend/gestor-inventario-frontend/src/app/app.routes.ts
+++ frontend/gestor-inventario-frontend/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { DashboardAdmin } from './pages/dashboard-admin/dashboard-admin';
+import { Home } from './pages/home/home';
+import { QuienesSomos } from './pages/quienes-somos/quienes-somos';
 
 export const routes: Routes = [
     { path: "dashboardAdmin", component: DashboardAdmin },
+    { path: "", component: Home },
+    { path: "QuienesSomos", component: QuienesSomos },
 ];
