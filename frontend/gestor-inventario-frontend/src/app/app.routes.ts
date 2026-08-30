272531d7eeca0642dafdaa6e84e6ce99352fe71d
MateCooked <mattecooked@gmail.com>
2026-08-30 00:56:05 -0300

    [Matias] feat: registrar ruta dashboardEmpleado y routerOutlet

--- frontend/gestor-inventario-frontend/src/app/app.routes.ts
+++ frontend/gestor-inventario-frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { DashboardAdmin } from './pages/dashboard-admin/dashboard-admin';
+import { DashboardEmpleado } from './pages/dashboard-empleado/dashboard-empleado';
 
 export const routes: Routes = [
     { path: "dashboardAdmin", component: DashboardAdmin },
+    { path: "dashboardEmpleado", component: DashboardEmpleado}
 ];
--- frontend/gestor-inventario-frontend/src/app/pages/dashboard-empleado/dashboard-empleado.ts
+++ frontend/gestor-inventario-frontend/src/app/pages/dashboard-empleado/dashboard-empleado.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-empleado',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './dashboard-empleado.html',
   styleUrl: './dashboard-empleado.css',
 })
