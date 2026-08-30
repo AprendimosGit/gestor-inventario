52db61ede36ca55a0ae5b37d481d9286829d69a0
MateCooked <mattecooked@gmail.com>
2026-08-30 02:24:33 -0300

    [Matias] feat: Se agregan validaciones nativas de Angular  y mensajes de error en registrar-venta

--- frontend/gestor-inventario-frontend/src/app/pages/dashboard-empleado/registrar-venta/registrar-venta.ts
+++ frontend/gestor-inventario-frontend/src/app/pages/dashboard-empleado/registrar-venta/registrar-venta.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-venta',
@@ -11,7 +11,15 @@ export class RegistrarVenta {
   private fb = inject(FormBuilder);
 
   ventaForm = this.fb.group({
-    producto: [''],
-    cantidad: [''],
+    producto: ['', [Validators.required]],
+    cantidad: ['', [Validators.required , Validators.min(1), Validators.max(50)]],
   });
+
+  get producto(){
+    return this.ventaForm.controls.producto;
+  }
+
+  get cantidad() {
+    return this.ventaForm.controls.cantidad;
+  }
 }
