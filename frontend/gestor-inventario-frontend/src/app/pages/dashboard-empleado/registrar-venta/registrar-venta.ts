36868271596babbd5ea23d696fae9eadef5ea64b
MateCooked <mattecooked@gmail.com>
2026-08-30 02:41:49 -0300

    [Matias] feat: se conecta submit de formulario de registrar-venta con la logica de negocio

--- frontend/gestor-inventario-frontend/src/app/pages/dashboard-empleado/registrar-venta/registrar-venta.ts
+++ frontend/gestor-inventario-frontend/src/app/pages/dashboard-empleado/registrar-venta/registrar-venta.ts
@@ -1,6 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
+interface ProductoVenta {
+  nombre: string;
+  stockDisponible: number;
+}
+
 @Component({
   selector: 'app-registrar-venta',
   imports: [ReactiveFormsModule],
@@ -10,10 +15,20 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 export class RegistrarVenta {
   private fb = inject(FormBuilder);
 
+  productos: ProductoVenta[] = [
+    { nombre: 'Pizza Muzzarella', stockDisponible: 10},
+    { nombre: 'Pizza Especial', stockDisponible: 6},
+    { nombre: 'Pizza Napolitana', stockDisponible: 8},
+    { nombre: 'Lomo Simple', stockDisponible: 12},
+    { nombre: 'Lomo Completo', stockDisponible: 3},
+  ];
+
   ventaForm = this.fb.group({
     producto: ['', [Validators.required]],
-    cantidad: ['', [Validators.required , Validators.min(1), Validators.max(50)]],
+    cantidad: [1, [Validators.required , Validators.min(1), Validators.max(50)]],
   });
+
+  ventaRegistrada = false;
 
   get producto(){
     return this.ventaForm.controls.producto;
@@ -21,5 +36,16 @@ export class RegistrarVenta {
 
   get cantidad() {
     return this.ventaForm.controls.cantidad;
+  }
+
+  registrarVenta(): void {
+    if (this.ventaForm.invalid) {
+      this.ventaForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Venta registrada: ', this.ventaForm.value);
+    this.ventaRegistrada = true;
+    this.ventaForm.reset();
   }
 }
