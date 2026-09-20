99c5c794930b80c35f32e733292978dc0d0ba19f
fernandallanos13 <llanosfernanda1@gmail.com>
2026-09-19 23:59:11 -0300

    feat: conectar ProductoService con HttpClient al dashboard admin

--- frontend/gestor-inventario-frontend/src/app/pages/dashboard-admin/dashboard-admin.ts
+++ frontend/gestor-inventario-frontend/src/app/pages/dashboard-admin/dashboard-admin.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProductoService } from '../../services/producto.service';
+import { Producto } from '../../models/producto.model';
 
 @Component({
   selector: 'app-dashboard-admin',
@@ -8,18 +10,26 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   templateUrl: './dashboard-admin.html',
   styleUrl: './dashboard-admin.css',
 })
-export class DashboardAdmin {
+export class DashboardAdmin implements OnInit {
   form: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+  productos: Producto[] = [];
+
+  constructor(private formBuilder: FormBuilder, private productoService: ProductoService) {
     this.form = this.formBuilder.group({
-  producto: ['', [Validators.required]],
-  cantidad: ['', [Validators.required]],
-  proveedor: ['', [Validators.required]],
-  unidad: ['', [Validators.required]]
-});
+      producto: ['', [Validators.required]],
+      cantidad: ['', [Validators.required]],
+      proveedor: ['', [Validators.required]],
+      unidad: ['', [Validators.required]]
+    });
+  }
+
+  ngOnInit(): void {
+    this.productoService.getProductos().subscribe((data) => {
+      this.productos = data;
+    });
   }
 
   onSubmit() {
-  console.log(this.form.value);
-}
+    console.log(this.form.value);
+  }
 }
