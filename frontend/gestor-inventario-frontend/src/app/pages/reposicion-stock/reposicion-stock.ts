adfb0a89a28facc6cdc3599bdb826abc81b87527
MateCooked <mattecooked@gmail.com>
2026-09-22 14:29:17 -0300

    feat: Se agrega diseño de formulario y css responsive

--- frontend/gestor-inventario-frontend/src/app/pages/reposicion-stock/reposicion-stock.ts
+++ frontend/gestor-inventario-frontend/src/app/pages/reposicion-stock/reposicion-stock.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reposicion-stock',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './reposicion-stock.html',
   styleUrl: './reposicion-stock.css',
 })
-export class ReposicionStock {}
+export class ReposicionStock {
+    form: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.form = this.fb.group({
+            ingrediente: [''],
+            cantidad: [''],
+            proveedor: ['']
+        })
+    }
+}
