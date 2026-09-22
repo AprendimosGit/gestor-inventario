2434446eea56aabecefc3f4bf07c65aa01fb55fd
MateCooked <mattecooked@gmail.com>
2026-09-22 18:32:53 -0300

    feat: Validacion de credenciales en el Login

--- frontend/gestor-inventario-frontend/src/app/pages/login/login.ts
+++ frontend/gestor-inventario-frontend/src/app/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +10,13 @@ import { Router, RouterLink } from '@angular/router';
   styleUrl: './login.css',
 })
 export class Login {
+  private usuariosService = inject(UsuarioService);
   private fb = inject(FormBuilder);
   private router = inject(Router);
 
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(5)]],
-    rol: ['empleado', Validators.required],
   });
 
   get email() {
@@ -31,13 +32,29 @@ export class Login {
       this.loginForm.markAllAsTouched();
       return;
     }
+    const email = this.loginForm.controls.email.value;
+    const password = this.loginForm.controls.password.value;
 
-    const rolSeleccionado = this.loginForm.controls.rol.value;
-
-    if (rolSeleccionado === 'admin') {
-      this.router.navigate(['/dashboard-admin']);
-    } else {
-      this.router.navigate(['/dashboard-empleado']);
-    }
+    this.usuariosService.getUsuarios().subscribe({
+      next: (usuarios) => {
+        const usuario = usuarios.find(
+          u => u.mail === email && u.password === password
+        );
+        if (!usuario) {
+          alert('Correo o contraseña incorrectos.');
+          return;
+        }
+        console.log('Usuario verificado ', usuario);
+        if (usuario.id_rol === 1) {
+          this.router.navigate(['/dashboard-admin']);
+        } else if (usuario.id_rol === 2 || usuario.id_rol === 3) {
+          this.router.navigate(['/dashboard-empleado']);
+        }
+      },
+      error: (error) => {
+        console.error('Error al consultar usuarios: ', error)
+        alert('No se pudo conectar con el servidor');
+      }
+    });
   }
 }
