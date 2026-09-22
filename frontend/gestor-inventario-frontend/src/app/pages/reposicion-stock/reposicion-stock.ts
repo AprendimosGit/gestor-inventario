e39e40727bb5cceac80eecd1bc60c34deefc4aef
MateCooked <mattecooked@gmail.com>
2026-09-22 15:53:37 -0300

    feat: carga de ingredientes, proveedores y configuracion de validaciones de stock

--- frontend/gestor-inventario-frontend/src/app/pages/reposicion-stock/reposicion-stock.ts
+++ frontend/gestor-inventario-frontend/src/app/pages/reposicion-stock/reposicion-stock.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { StockService } from '../../services/stock.service';
+import { Ingrediente } from '../../models/ingrediente.model';
+import { Proveedor } from '../../models/proveedor.model';
 
 @Component({
   selector: 'app-reposicion-stock',
@@ -8,14 +11,36 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './reposicion-stock.html',
   styleUrl: './reposicion-stock.css',
 })
-export class ReposicionStock {
+export class ReposicionStock implements OnInit {
     form: FormGroup;
+    ingredientes: Ingrediente[] = [];
+    proveedores: Proveedor[] = [];
 
-    constructor(private fb: FormBuilder) {
+    constructor(private fb: FormBuilder, private stockService:StockService , private cdr: ChangeDetectorRef) {
         this.form = this.fb.group({
-            ingrediente: [''],
-            cantidad: [''],
-            proveedor: ['']
+            ingrediente: ['', Validators.required],
+            cantidad: ['', [Validators.required, Validators.min(1)]],
+            proveedor: ['', Validators.required]
         })
+    }
+    ngOnInit(): void {
+        this.stockService.getIngredientes().subscribe({
+            next: (data) => {
+                this.ingredientes = data;
+                this.cdr.detectChanges();
+            },
+            error: (error) => {
+                console.error("Error al cargar ingredientes", error)
+            }
+        });
+        this.stockService.getProveedores().subscribe({
+            next: (data) => {
+                this.proveedores = data;
+                this.cdr.detectChanges();
+            },
+            error: (error) => {
+                console.error("Error al cargar proveedores: ", error);
+            }
+        });
     }
 }
